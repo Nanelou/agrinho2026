9eec607d6aed3bcb44f1eee39b16a1d0b10f20d5
NaNeLou <126290968+Nanelou@users.noreply.github.com>
2026-06-07 22:39:02 -0300

    agr2026

---
+++ main.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.innerHTML =
+    "Produzir alimentos e preservar a natureza são objetivos que podem caminhar juntos. A sustentabilidade é o caminho para um agro forte e um futuro melhor para todos.";
+
+}
